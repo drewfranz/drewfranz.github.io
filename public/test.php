b370b4f17c2a05552acdd6adda00e40e333855fa
Drew Franz <drewfranz@gmail.com>
2011-11-10 12:46:54 -0800

    added chapter2 & chapter3 example folders and files

---
+++ public/test.php
@@ -0,0 +1,13 @@
+<?php
+
+   echo  "Hello, World!";
+
+   $myVar = 5;
+
+   if($myVar<0){
+      echo "my variable is less than 4!";
+   } else {
+      echo "my variable is greater than 4!";
+   }
+
+?>
